81b91504ae201db96450c70871651abc6d392bb0
initial <gy655941@dal.ca>
2025-10-19 14:25:50 -0300

    php

--- db.php
+++ db.php
@@ -1,6 +1,26 @@
 <?php
-    $conn = new mysqli("localhost", "root","","it_helpdesk");
-    if($conn -> connect_error){
-        die("connect failed!". $conn->connect_error);
+
+    $db_host = getenv('MYSQLHOST'); 
+
+    $db_user = getenv('MYSQLUSER'); 
+
+  
+    $db_pass = getenv('MYSQL_ROOT_PASSWORD'); 
+
+    $db_name = getenv('MYSQL_DATABASE'); 
+
+    $db_port = getenv('MYSQLPORT'); 
+
+    
+    
+    $conn = new mysqli($db_host, $db_user, $db_pass, $db_name, (int)$db_port);
+
+    if ($conn->connect_error) {
+        die("Connect failed! Error: " . $conn->connect_error);
     }
+    
+
+    $conn->set_charset("utf8mb4");
+
+    // ...
 ?>
