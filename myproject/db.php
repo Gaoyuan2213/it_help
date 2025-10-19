3245465cb3ab6116bd00be836ca26ed30ca4e55a
initial <gy655941@dal.ca>
2025-10-19 18:02:59 -0300

    edit db.php

--- myproject/db.php
+++ myproject/db.php
@@ -1,25 +1,30 @@
 <?php
 
-    $db_host = getenv('MYSQLHOST'); 
 
-    $db_user = getenv('MYSQLUSER'); 
+    $db_host = getenv('MYSQL_HOST'); 
 
-  
-    $db_pass = getenv('MYSQL_ROOT_PASSWORD'); 
+    $db_user = getenv('MYSQL_USER'); 
+
+
+    $db_pass = getenv('MYSQL_PASSWORD'); 
 
     $db_name = getenv('MYSQL_DATABASE'); 
 
-    $db_port = getenv('MYSQLPORT'); 
+    $db_port = getenv('MYSQL_PORT'); 
 
+    if (!$db_host || !$db_user || !$db_pass || !$db_name) {
+ 
+        die("Error: Required database environment variables are missing.");
+    }
     
     
     $conn = new mysqli($db_host, $db_user, $db_pass, $db_name, (int)$db_port);
 
     if ($conn->connect_error) {
+   
         die("Connect failed! Error: " . $conn->connect_error);
     }
     
-
     $conn->set_charset("utf8mb4");
 
     // ...
