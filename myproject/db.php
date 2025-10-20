677417f8b27ff0996f1b43d137a9d7c1b95e6066
initial <gy655941@dal.ca>
2025-10-20 12:19:41 -0300

    modify db

--- myproject/db.php
+++ myproject/db.php
@@ -1,21 +1,21 @@
 <?php
-// InfinityFree 数据库连接信息
-$db_host = "sql113.infinityfree.com"; // MySQL Host Name
-$db_user = "if0_40208890";           // MySQL User Name
-$db_pass = "(Your vPanel Password)";  // MySQL Password，填你在 InfinityFree 的 vPanel 密码
-$db_name = "if0_40208890_helpDesk";   // MySQL DB Name
-$db_port = 3306;                      // MySQL 默认端口
 
-// 尝试连接
+$db_host = "sql113.infinityfree.com"; 
+$db_user = "if0_40208890";           
+$db_pass = "7CNxUs0EiOj"; 
+$db_name = "if0_40208890_helpDesk";   
+$db_port = 3306;                      
+
+
 $conn = new mysqli($db_host, $db_user, $db_pass, $db_name, $db_port);
 
-// 检查连接是否成功
+
 if ($conn->connect_error) {
     die("Connect failed! Error: " . $conn->connect_error);
 }
 
-// 设置字符集
+
 $conn->set_charset("utf8mb4");
 
-// 现在你可以正常使用 $conn 执行数据库操作
+
 ?>
